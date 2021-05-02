7c16e9d29684075aef8132a385fff85bee1406da
Killa J <joshuaalexandercross@gmail.com>
2021-05-01 23:29:15 -0500

    refactored code by creating seperate route files for api requests

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+// Centralized hub to pull candidate, party and voter js files together
+
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+
+module.exports = router;
